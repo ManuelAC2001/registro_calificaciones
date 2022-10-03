de3a7e646d15a70725d7a96db241e6010525f529
Robert <roberttv17@gmail.com>
2022-10-02 20:30:37 -0500

    Funcion generar codig creada

--- src/main/java/control_calificaciones/Test.java
+++ src/main/java/control_calificaciones/Test.java
@@ -1,12 +1,16 @@
 package control_calificaciones;
 
+import control_calificaciones.helpers.Helpers;
 import control_calificaciones.helpers.emails.EnviarEmails;
 
 public class Test {
 
     public static void main(String[] args) {
-        EnviarEmails enviarEmail = new EnviarEmails();
-        enviarEmail.enviarEmail();
+      /*   EnviarEmails enviarEmail = new EnviarEmails();
+        enviarEmail.enviarEmail(); */
+
+        System.out.println( Helpers.generarCodigo() );
+        
     }
 
 }
