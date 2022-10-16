45bee9b5c07f18b610aa2f272ddc53a00ef8d37c
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2022-10-16 13:11:24 -0500

    html to pdf

--- src/main/java/control_calificaciones/Test.java
+++ src/main/java/control_calificaciones/Test.java
@@ -1,10 +1,28 @@
 package control_calificaciones;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.openhtmltopdf.pdfboxout.PdfRendererBuilder;
+
+
 public class Test {
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args) {
+        String filePath = System.getProperty("user.dir");
+
+        File htmlSource = new File(filePath + "\\index.html");
+        File pdfDest = new File(filePath + "\\index.pdf");
+
+        try (OutputStream os = new FileOutputStream(pdfDest)) {
+            PdfRendererBuilder builder = new PdfRendererBuilder();
+            builder.useFastMode();
+            builder.withFile(htmlSource);
+            builder.toStream(os);
+            builder.run();
+        }
         
-
     }
-
 }
