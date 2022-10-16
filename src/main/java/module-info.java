a1a1a6a18d762218b79a94179d8d8b21104d8caa
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2022-10-16 13:12:29 -0500

    html to pdf

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,14 +1,18 @@
 module control_calificaciones {
+    
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
     requires java.mail;
     requires java.dotenv;
     requires itextpdf;
-
-    //implementacion de hibernate
-    requires jakarta.persistence;
-    
+    requires openhtmltopdf.core;
+    requires openhtmltopdf.java2d;
+    requires openhtmltopdf.mathml.support;
+    requires openhtmltopdf.pdfbox;
+    requires openhtmltopdf.rtl.support;
+    requires openhtmltopdf.slf4j;
+    requires openhtmltopdf.svg.support;
 
     opens control_calificaciones.controllers to javafx.fxml;
     opens control_calificaciones.models to javafx.base;
