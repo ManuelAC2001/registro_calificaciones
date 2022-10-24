e0d4b483146b1db38cfc900e4debb211cd38b080
Manuel Apolinar Castillo <apolinar121070@gmail.com>
2022-10-24 16:42:38 -0500

    pasando a la version con hibernate del proyecto

--- src/main/java/control_calificaciones/Test.java
+++ src/main/java/control_calificaciones/Test.java
@@ -18,7 +18,7 @@ public class Test {
     public static void main(String[] args) throws IOException {
 
         generarListaPDF();
-
+        
     }
 
     public static void agregarAlumno(Document documentHTML) {
